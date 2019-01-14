c061feae7366891485c48dc4783f377aca804b1d
John <johnsmilga@gmail.com>
2019-01-14 03:28:56 -0800

    first commit

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,13 +1,9 @@
 const Product = require("../models/product");
 
 module.exports.getIndex = (req, res, next) => {
-  Product.find()
-    .then(products => {
-      res.render("index", {
-        products
-      });
-    })
-    .catch(err => console.log(err));
+  res.render("index", {
+    products: []
+  });
 };
 module.exports.getAbout = (req, res, next) => {
   res.render("about");
@@ -16,19 +12,5 @@ module.exports.getAddProduct = (req, res, next) => {
   res.render("add-product");
 };
 module.exports.postAddProduct = (req, res, next) => {
-  const name = req.body.name;
-  const image = req.body.image;
-  const price = req.body.price;
-  // console.log(name, image, price);
-
-  const product = new Product({ name, image, price });
-
-  product
-    .save()
-    .then(result => {
-      // console.log(result);
-
-      res.redirect("/");
-    })
-    .catch(err => console.log(err));
+  res.redirect("/");
 };
