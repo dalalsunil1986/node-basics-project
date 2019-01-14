5611027473e47481e6d487f161e5496ecc347277
John <johnsmilga@gmail.com>
2019-01-14 03:33:47 -0800

    post added

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -12,5 +12,19 @@ module.exports.getAddProduct = (req, res, next) => {
   res.render("add-product");
 };
 module.exports.postAddProduct = (req, res, next) => {
-  res.redirect("/");
+  const name = req.body.name;
+  const image = req.body.image;
+  const price = req.body.price;
+  // console.log(name, image, price);
+
+  const product = new Product({ name, image, price });
+
+  product
+    .save()
+    .then(result => {
+      // console.log(result);
+
+      res.redirect("/");
+    })
+    .catch(err => console.log(err));
 };
